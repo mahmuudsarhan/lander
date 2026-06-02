e685a09bc720f684501d8a0331511ac92757cf7a
! <mahmuudsarhan@gmail.com>
2026-06-02 22:30:47 +0300

    After edits

---
+++ README.md
@@ -0,0 +1,22 @@
+# Lander
+
+You control a spacecraft to land on the moon without smashing it like a meteor (Funny wat practice free fall equations)
+
+```text
+           /\           ==================================
+          //\\            TIME WASTED: 14 seconds
+         ||##||         ==================================
+        //##mm\\        ALTITUDE [########   ] 642 meters
+       //##*mmm\\       Speed [===        ] 43 m/s
+      //###**mmm\\      Engine Food [###########] 180 Liters
+```
+## Wanna try it?
+* **On windows you can download it an run it locally**
+* **For other operating systems, you can try it online**
+
+### For players (Windows)
+1. Download `lander.exe`
+2. Launch the file (It will open the terminal automatically)
+3. Throw down a random number, hit Enter, and start playing
+
+### For players (Non-Windows OS)
--- main.cpp
+++ main.cpp
@@ -1,107 +1,178 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
-#include <cmath>
-#include <algorithm>
+#include <windows.h>
+#include <stdio.h> // I need it just cuz using printf looks classy
+
 using namespace std;
 
-void clearScrean() {
-#ifdef _WIN32
-    system("cls");
-#else 
-    system("clear");
-#endif
-}
-
-void drawgauge(string label, double value, double maxVal, string unit) {
-    const int barWidth = 20;
-    double percentage = value / maxVal;
-    if (percentage > 1.0) percentage = 1.0;
-    if (percentage < 0.0) percentage = 0.0;
-    
-    if (label == "FUEL" && percentage < 0.2){
-        cout << "\033[1;31m";
-    } else {
-        cout << "\033[1;32m";
-    } cout << "\033[0m";
-
-    int progress = static_cast<int>(barWidth * percentage);
-    cout << left << setw(12) << label << " [";
-    for (int i = 0; i < barWidth; ++i) {
-        if (i < progress) cout << "#";
-        else cout << " ";
-    }
-    cout << "] " << value << " " << unit << "\n";
-    
-}
+// I can't rememebr what th is this doing but if it works never touch it
+int delete_this_and_the_game_splodes = 420; 
 
 int main() {
-    double myAltitude = 1000.0;
-    double myVelocity = 50.0;
-    double fuel = 150.0;
-    double gravity = 1.62;
-    int timePassed = 0;
-    double PlayerBurn = 0.0;
+	// You just need to play with a full screen to enjoy
+	ShowWindow(GetConsoleWindow(), 3); 
 
-    clearScrean();
+	// The main variables
+	double how_high_we_are = 1000.0; 
+	double zoom_zoom_speed = 50.0;   
+	double spicy_juice = 220.0; 
+	int eternal_suffering_ticks = 0;       
+	double oh_god_how_much_fuel_are_we_burning = 0; 
+	int score_placeholder_idk = 0; 
 
-    cout << "==================================================\n";
-    cout << "                 WELCOME TO MY GAME               \n";
-    cout << "==================================================\n";
-    cout << "You must land with a speed less than 5 (obviously)\n";
-    cout << "Type a number and hit enter if you want to play\n";
+	system("cls");
+	OUT_STREAM_HACK: // -- I don't need labels to understand the code but I felt bad when I kept it that messy
+	cout << "=============================================\n"
+	    << "                 Moon Lander                  \n"
+	    << "=============================================\n"
+	    << "Land softly (<5 speed) or give the moon a new crater.\n"
+	    << "Slap a number and smash Enter: ";
 
-    double startInput;
-    if (!(cin >> startInput)) return 0;
+	double meaningless_starter_garbage;
+	if (!(cin >> meaningless_starter_garbage)) { return -999; } // angry error return
 
-    while (myAltitude > 0) {
-        timePassed += 1;
-        cout << "\n===========================================\n";
-        cout << "Time PAST: " << timePassed << " seconds\n"; 
-        cout << "=============================================\n";
+	while (how_high_we_are > 0.0) {
+		eternal_suffering_ticks++;
+		system("cls");
 
-        drawgauge("ALTITUDE", myAltitude, 1000.0, "meters");
-        drawgauge("SPEED", myVelocity, 100.0, "m/s");
-        drawgauge("FUEL", fuel, 150.0, "liters");
-        cout << "---------------------------------------------\n";
+		// terminal frame split
+		cout << R"(           /\           )" << "\t==================================\n";
+	cout << R"(          //\\          )" << "\t  TIME WASTED: " << eternal_suffering_ticks << " seconds\n";
+		cout << R"(         ||##||         )" << "\t==================================\n";
+		
+		// Altitude Bar 
+		cout << R"(        //##mm\\        )" << "\t\033[1;32mALTITUDE [";
+		if (how_high_we_are > 800)      cout << "###########";
+		else if (how_high_we_are > 600) cout << "########   ";
+		else if (how_high_we_are > 400) cout << "#####      ";
+		else if (how_high_we_are > 200) cout << "##         ";
+		else                           cout << "           ";
+		printf("] %d meters\033[0m\n", (int)how_high_we_are);
 
-        if (fuel > 0) {
-            cout << "How much fuel do you want to burn (0 to 20): ";
-            cin >> PlayerBurn;
+		// Speed Bar
+		cout << R"(       //##*mmm\\       )" << "\t\033[1;32mSpeed [";
+	if (zoom_zoom_speed > 80)       cout << "###########";
+	else if (zoom_zoom_speed > 60)  cout << "########   ";
+	else if (zoom_zoom_speed > 40)  cout << "===        "; 
+	else if (zoom_zoom_speed > 20)  cout << "##         ";
+	else                           cout << "           ";
+		printf("] %d m/s\033[0m\n", (int)zoom_zoom_speed);
 
-            if (PlayerBurn < 0) PlayerBurn = 0;
-            if (PlayerBurn > 20) PlayerBurn = 20;
-            if (PlayerBurn > fuel) PlayerBurn = fuel;
+		// Fuel Bar
+		cout << R"(      //###**mmm\\      )" << "\t" << (spicy_juice < 40.0 ? "\033[1;31m" : "\033[1;32m") << "Engine Food [";
+		if (spicy_juice > 160)      cout << "###########";
+		else if (spicy_juice > 120) cout << "########   ";
+		else if (spicy_juice > 80)  cout << "#####      ";
+		else if (spicy_juice > 40)  cout << "##         ";
+		else                       cout << "           ";
+		printf("] %d Liters\033[0m\n", (int)spicy_juice);
+		
+		// Our Rookie Rocketo Rocket
+		cout << R"(     //###***nmmm\\     )" << "\t----------------------------------\n";
+	cout << R"(    //####***@nmmm\\    )" << "\tMAYDAY LOG:\n";
+		cout << R"(    ||####***@nnmm||    )" << "\t- 4 fuel keeps the same acceleration\n";
+	cout << R"(    ||####**@@@nnm||    )" << "\t- Try to free fall then smash that engine\n";
+		cout << R"(    |______________|    )" << "\t----------------------------------\n";
+		cout << R"(    |     Lander   |    )" << "\n";
+		cout << R"(     \____________/     )" << "\n";
+	cout << R"(      |          |      )" << "\n";
+		cout << R"(     /|    /\    |\     )" << "\n";
+		cout << R"(    /_|    || /\ |_\    )" << "\n";
+		cout << R"(      |      Moon|      )" << "\n";
+		cout << R"(      |       \/ |      )" << "\n";
+		cout << R"(      |          |      )" << "\n";
+		cout << R"(     /|    /\    |\     )" << "\n";
+		cout << R"(    / |    ||    | \    )" << "\n";
+		cout << R"(   /  |    ||    |  \   )" << "\n";
+		cout << R"(  /  /\    ||    /\  \  )" << "\n";
+		cout << R"( |__/  \   ||   /  \__| )" << "\n";
+		cout << R"(   /____\      /____\   )" << "\n";
+		cout << R"(   |    |      |    |   )" << "\n";
+		cout << R"(   |    |______|    |   )" << "\n";
+		cout << R"(   |    | /--\ |    |   )" << "\n";
+		cout << R"(   |____|/----\|____|   )" << "\n";
 
-            fuel -= PlayerBurn;
-        } else {
-            cout << "You are out of fuel, dude (Press Enter to drop) \n";
-            PlayerBurn = 0;
-        }
-    
-        double thrustForce = PlayerBurn * 0.4;
-        double acceleration = gravity - thrustForce;
-        myVelocity += acceleration;
-        myAltitude -= myVelocity;
+		// Fire it Burn it enjoy the life
+		if (oh_god_how_much_fuel_are_we_burning > 15.0) {
+			if (eternal_suffering_ticks % 2 == 0) {
+				cout << R"(    \||/ //##\\ \||/    )" << "\n";
+				cout << R"(    /##\//####\\/##\    )" << "\n";
+				cout << R"(   //##\\/####\//##\\   )" << "\n";
+				cout << R"(  ||/::\||/##\||/::\||  )" << "\n";
+				cout << R"(   \\\///\::::/\\\///   )" << "\n";
+			} else {
+				cout << R"(     \|/  //##\\  \|/   )" << "\n";
+				cout << R"(    /##\//####\\/##\    )" << "\n";
+				cout << R"(   //##\\/####\//##\\   )" << "\n";
+				cout << R"(  ||/::\||/##\||/::\||  )" << "\n";
+				cout << R"(    \\//\\\::///\\//    )" << "\n";
+			}
+		} else if (oh_god_how_much_fuel_are_we_burning > 5.0) {
+			cout << R"(        //##\\          )" << "\n";
+			cout << R"(       //####\\         )" << "\n";
+			cout << R"(       \\:::://         )" << "\n";
+			cout << R"(        \\//            )" << "\n";
+		} else if (oh_god_how_much_fuel_are_we_burning > 0.0) {
+			cout << R"(          ||            )" << "\n";
+			cout << R"(          \/            )" << "\n";
+		} else {
+			cout << "\n\n\n\n\n"; 
+		}
 
-        if (myAltitude > 2000.0) {
-            cout << "\033[1;31m You are lost in space, Game Over \033[0m\n";
-        }
-    }
-    clearScrean();
-    cout << "==================================================\n";
-    cout << "              Your Final Reuslts                  \n";
-    cout << "==================================================\n";
-    cout << "Final Speed: " << myVelocity << " m/s\n";
-    cout << "Fuel Left :" << fuel << " liters\n\n";
+		cout << "------------------------------------------------------------------------\n";
+		if ((how_high_we_are / 1000.0) <= 0.12) {
+			cout << "Dude, imagine that the universe's weakest force just beat you\n";
+		} else {
+			cout << "Just Trying to land safely with that piece of iron\n";
+		}
 
-    if (myVelocity <= 5.0) {
-        cout << "YOU WIN!!!!!!!";
-    } else {
-        cout << "Loser, you crashed like kaboom";
-    }
-    cout << "===================================================\n";
+		if (spicy_juice > 0.0) {
+			cout << "\nHow much fuel are we spraying? (0-20): ";
+			cin >> oh_god_how_much_fuel_are_we_burning;
 
-    return 0;
+			// smash the keyboard recovery hack
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(99999, '\n');
+				oh_god_how_much_fuel_are_we_burning = 0;
+			}
+			if (oh_god_how_much_fuel_are_we_burning < 0) oh_god_how_much_fuel_are_we_burning = 0;
+			if (oh_god_how_much_fuel_are_we_burning > 20) oh_god_how_much_fuel_are_we_burning = 20;
+			if (oh_god_how_much_fuel_are_we_burning > spicy_juice) oh_god_how_much_fuel_are_we_burning = spicy_juice;
+
+			spicy_juice -= oh_god_how_much_fuel_are_we_burning;
+		} else {
+			cout << "\033[1;31mTHE TANK IS BONE DRY! Smash Enter to embrace gravity... \033[0m\n";
+			cin.ignore(99999, '\n');
+			cin.get();
+			oh_god_how_much_fuel_are_we_burning = 0;
+		}
+	
+		// Math update loop (using lazy unoptimized constants)
+		zoom_zoom_speed += (1.62 - (oh_god_how_much_fuel_are_we_burning * 0.4));
+		how_high_we_are -= zoom_zoom_speed;
+
+		if (how_high_we_are > 2500.0) {
+			cout << "\033[1;31mYou missed the moon completely. Enjoy floating forever. Bye. GG.\033[0m\n";
+			return 0;
+		}
+	}
+
+	system("cls");
+	cout << "====================================\n"
+		 << "     POST-CRASH DAMAGE REPORT       \n"
+		 << "====================================\n";
+	printf("Smash Velocity:   %.2f m/s\n", zoom_zoom_speed);
+	printf("Leftover Rocket Food: %.2f L\n\n", spicy_juice);
+
+	if (zoom_zoom_speed <= 5.0 && zoom_zoom_speed >= 0.0) {
+		cout << "Task failed successfully! The ship is intact and the aliens are clapping (You win).\n";
+	} else {
+		cout << "KABOOM! You just turned a multi-billion dollar lander into confetti lol\n";
+	}
+
+	cout << "\nHit Enter to destroy this console window...";
+	cin.ignore(99999, '\n');
+	cin.get();
+	return 0;
 }
-
